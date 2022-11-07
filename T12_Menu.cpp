b70a393403a415e43ec643ee76ee9a9d00377811
Manuel Rojas <manuerjs70@gmail.com>
2022-11-07 15:37:58 -0600

    Se agrega ejercicio de menu en c++

---
+++ T12_Menu.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    bool salir = false;
+    while (!salir)
+    {
+        int opcion;
+        cout << "\n--------MENU--------\n";
+        cout << "1. Primera opción\n2. Segunda opción\n3. Tercera opción\n4.Salir\n\n";
+        cout << "Ingrese una opción: ";
+        cin >> opcion;
+        if (opcion == 1)
+        {
+            cout<<"Has elegido la primera opcion!\n";
+        }else if (opcion == 2)
+        {
+            cout<<"Has elegido la segunda opcion!\n";
+        }else if (opcion == 3)
+        {
+            cout<<"Has elegido la tercera opcion!\n";
+        }else if (opcion == 4)
+        {
+            salir = true;
+        }else
+        {
+            cout<<"Esa no es una opción válida";
+        }
+    }
+    cout<<"Has salido del programa! Nos vemos";
+    
+    return 0;
+}
