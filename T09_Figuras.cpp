b105f21bf63989bdb6160682206573ba12084f65
Manuel Rojas <manuerjs70@gmail.com>
2022-11-13 20:20:43 -0600

    Ejercicio sobre figuras con signos de escape

---
+++ T09_Figuras.cpp
@@ -0,0 +1,12 @@
+// 202031936 MADHELEIN LOURDES SOCH ORDONEZ
+//"Figuras con signos de escape"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //figura 1 y 2
+    cout<<"*************************\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*************************\n\n**\n  **\n    **\n      **\n        **\n          **\n            **\n              **\n******************";
+    return 0;
+}
